30fac2a8a156ea89a696acdacaeac6012ab090a2
DBaker85 <badcancer22@gmail.com>
2017-07-13 10:38:58 +0200

    configure AOT

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowser } from '@angular/platform-browser';
 import './polyfills.ts'
-import { AppModuleNgFactory } from '../../public/aot/src/app/app.module.ngfactory';
+import { AppModuleNgFactory } from '../aot/src/app/app.module.ngfactory';
 
 
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
