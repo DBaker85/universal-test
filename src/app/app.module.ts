faf09678ee7b4c494b66073c973b244bf7eb3cda
David Baker <d.baker@vlkextern.nl>
2017-08-10 13:59:11 +0200

    remove unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search.component';
-import { OnlineListComponent } from './online-list/onlineList.component';
-import { NewsComponent } from './news/news.component';
-import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SearchComponent,
-    OnlineListComponent,
-    NewsComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
